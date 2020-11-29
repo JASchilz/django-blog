b47dd427d8a53e7d30c79f68f5263838d2ecc519
Joseph Schilz <joseph@schilz.org>
2020-11-28 23:11:49 -0800

    Update views.py
    Great work! My rule of thumb, especially when working on production code, is to remove old code rather than comment it out. That way, it isn't cluttering up your files, and if you ever want to get that code back then you can retrieve it from your commit history.

--- blogging/views.py
+++ blogging/views.py
@@ -7,30 +7,6 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
-
-
-# def list_view(request):
-#     published = Post.objects.exclude(published_date__exact=None)
-#     posts = published.order_by('-published_date')
-#     template = loader.get_template('blogging/list.html')
-#     context = {'posts': posts}
-#     body = template.render(context)
-#     return HttpResponse(body, content_type="text/html")
-# def list_view(request):
-#     published = Post.objects.exclude(published_date__exact=None)
-#     posts = published.order_by('-published_date')
-#     context = {'posts': posts}
-#     return render(request, 'blogging/list.html', context)
-
 
 class PostListView(ListView):
     # published = Post.objects.exclude(published_date__exact=None)
@@ -39,16 +15,6 @@ class PostListView(ListView):
         "-published_date"
     )
     template_name = "blogging/list.html"
-
-
-# def detail_view(request, post_id):
-#     published = Post.objects.exclude(published_date__exact=None)
-#     try:
-#         post = published.get(pk=post_id)
-#     except Post.DoesNotExist:
-#         raise Http404
-#     context = {'post': post}
-#     return render(request, 'blogging/detail.html', context)
 
 
 class PostDetailView(DetailView):
